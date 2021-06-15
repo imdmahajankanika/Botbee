1c1b78398c80823143b1f39aa3756d2e6e89dbfa
imdmahajankanika <48632517+imdmahajankanika@users.noreply.github.com>
2021-06-15 19:40:55 +0200

    Update index.js

--- fbeamer/index.js
+++ fbeamer/index.js
@@ -135,7 +135,7 @@ class FBeamer {
                     }) ;
                 }
                 else {
-                        console.log("errorrr: ",body.message_id,this.pageAccessToken);
+                        console.log("errorrr: ",body,response);
                         reject (error) ;
                 }
             }) ;
